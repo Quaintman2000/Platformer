03294662fa4abf316833a03e00d0a097fafb6dd4
Alec <Quaintman2000@gmail.com>
2020-03-16 12:25:24 -0700

    added project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform tf;
+    private Rigidbody2D rb2d;
+    private SpriteRenderer sr;
+
+    public float speed = 5;
+    public float jumpForce = 10;
+    public Transform groundPoint;
+    public bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = gameObject.GetComponent<Transform>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMovement = Input.GetAxis("Horizontal") * speed;
+        rb2d.velocity = new Vector2(xMovement, rb2d.velocity.y);
+        sr.flipY = rb2d.velocity.x < 0;
+        // if the player is on the ground
+        
+        RaycastHit2D hitInfo = Physics2D.Raycast(groundPoint.position, Vector2.down, 0.1f);
+        if (hitInfo.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        // jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb2d.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
